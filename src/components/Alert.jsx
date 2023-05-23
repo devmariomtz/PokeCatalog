11a7b5cdb744eb62bb7d640a1091907d58bf90f3
Martinez-Mario <you@example.com>
2023-05-22 19:46:30 -0600

    randomize and adding type in recommend pokemons grid

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Alert = ({text}) => {
   return (
-    <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-  <span class="font-medium">¡ATENCIÓN!</span>{text}
-</div>
-  )
+      <div className="flex justify-center ">
+        <div className="w-[95%] p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+          <span className="font-medium">¡ATENCIÓN!</span>{text}
+        </div>
+      </ div>
+
+  );
 }
 
 export default Alert
--- src/components/RecommendedPokemonsIMG.jsx
+++ src/components/RecommendedPokemonsIMG.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-
-
-const RecommendedPokemonsImg =  () => {
+const RecommendedPokemonsImg = () => {
     const [pokemonName, setPokemonName] = useState('');
     const [imgPokemonURL, setImgPokemonURL] = useState('');
-    const  pokemonId = 1;
+    const [pokeTypes, setPokeTypes] = useState('i');
+
+    const pokemonId = 1 + Math.round(Math.random() * 1010);
     useEffect(() => {
         const fetchPokemon = async () => {
             try {
                 const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-                const { name } = response.data;
+                const {name} = response.data;
                 setPokemonName(name);
             } catch (error) {
                 console.error('Error al obtener el nombre del Pokémon:', error);
             }
         };
         fetchPokemon();
-    }, [pokemonId]);
+    }, []);
 
     useEffect(() => {
         const fetchPokemonURL = async () => {
             try {
                 const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-                const { front_default } = response.data.sprites;
+                const {front_default} = response.data.sprites;
 
                 setImgPokemonURL(front_default);
             } catch (error) {
@@ -32,20 +32,35 @@ const RecommendedPokemonsImg =  () => {
             }
         };
         fetchPokemonURL();
-    }, [pokemonId]);
+    }, []);
+
+    useEffect(() => {
+        const fetchPokemonTypes = async () => {
+            try {
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+                const {types} = response.data;
+                setPokeTypes(types[0].type.name);
+
+
+            } catch (error) {
+                console.error('Error al obtener el nombre del Pokémon:', error);
+            }
+        };
+        fetchPokemonTypes();
+    }, []);
 
     return (
         <div className="lg:flex">
-            <img className="object-cover w-full h-56 rounded-lg lg:w-64"
-src={imgPokemonURL}                 alt="" />
+            <img className="object-cover w-full h-56 rounded-lg lg:w-64" src={imgPokemonURL} alt=""/>
 
-                <div className="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
-                        {pokemonName.toUpperCase()}
-                    </a>
+            <div className="flex flex-col justify-between py-6 lg:mx-6">
+                <a href="#" className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
+                    {pokemonName.toUpperCase()}
+                </a>
 
-                    <span className="text-sm text-gray-500 dark:text-gray-300">On: 20 October 2019</span>
-                </div>
+                <span
+                    className="text-[1em] text-gray-500 dark:text-gray-300">Type: {pokeTypes}</span>
+            </div>
         </div>
     );
 }
